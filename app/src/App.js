2970eafe13a1738516e885496845df3716d2c458
malhotratanuj <malhotra.tanuj@outlook.com>
2024-04-23 01:19:35 -0700

    first commit

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,6 @@ import { ethers } from 'ethers';
 import { useEffect, useState } from 'react';
 import deploy from './deploy';
 import Escrow from './Escrow';
-import { SyncLoader } from 'react-spinners';
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 
@@ -26,8 +25,8 @@ function App() {
   const [timelockValue, setTimelockValue] = useState('');
   const [showTimelockModal, setShowTimelockModal] = useState(false);
   const [contractToSetTimelock, setContractToSetTimelock] = useState(null);
-  const [disableApproveButton, setDisableApproveButton] = useState(false); // New state variable
-  const [showTimeSetPrompt, setShowTimeSetPrompt] = useState(false); // New state variable
+  const [disableApproveButton, setDisableApproveButton] = useState(false); 
+  const [showTimeSetPrompt, setShowTimeSetPrompt] = useState(false); 
 
   useEffect(() => {
     async function getAccounts() {
@@ -56,7 +55,6 @@ function App() {
   };
 
   const handleConfirmDelete = () => {
-    // index of the contract to delete
     const index = escrows.findIndex((escrow) => escrow.address === contractToDelete);
 
     if (index !== -1) {
@@ -72,8 +70,7 @@ function App() {
     const unlockTime = parseInt(timelockValue);
     if (!isNaN(unlockTime)) {
       await setTimelock(contractToSetTimelock, signer, unlockTime);
-      closeTimelockModal(); // Close the Timelock modal after setting the timelock
-      // amd ..Update the escrow object to indicate that the timelock is set
+      closeTimelockModal(); 
       const updatedEscrows = escrows.map((escrow) => {
         if (escrow.address === contractToSetTimelock) {
           return {
@@ -108,9 +105,9 @@ function App() {
       value: value.toString(),
       isTimelockSet: false, 
       handleApprove: async () => {
-        toggleLoading(true); // loading animation
+        toggleLoading(true); 
         escrowContract.on('Approved', () => {
-          toggleLoading(false); // Hide animation after approval
+          toggleLoading(false); 
           document.getElementById(escrowContract.address).className =
             'complete';
           document.getElementById(escrowContract.address).innerText =
@@ -133,82 +130,83 @@ function App() {
   };
 
   return (
-    <div className="container mx-auto px-4 py-4 ">
-      <div className="w-full sm:max-w-md mx-auto bg-white shadow-md rounded px-8 pt-4 pb-6 mb-4">
-        <h1 className="text-xl font-bold mb-2">New Contract</h1>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="arbiter">
-            Broker's Address
+    <div className="w-full">
+      <div className="w-full sm:max-w-md mx-auto bg-gray-200 shadow-lg rounded-lg px-6 py-8 mb-8">
+        <h1 className="text-3xl font-semibold mb-6 text-gray-700">Create New Contract</h1>
+        <div className="mb-6">
+          <label className="block text-gray-600 text-sm font-semibold mb-2" htmlFor="arbiter">
+            Broker
           </label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="arbiter" type="text" placeholder="Broker Address" />
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline" id="arbiter" type="text" placeholder="Enter Broker's Address" />
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="beneficiary">
-            Recipient's Address
+        <div className="mb-6">
+          <label className="block text-gray-600 text-sm font-semibold mb-2" htmlFor="beneficiary">
+            Recipient
           </label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="beneficiary" type="text" placeholder="Recipient Address" />
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline" id="beneficiary" type="text" placeholder="Enter Recipient's Address" />
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="wei">
-            Fund Amount (in Wei)
+        <div className="mb-6">
+          <label className="block text-gray-600 text-sm font-semibold mb-2" htmlFor="wei">
+            Amount
           </label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="wei" type="text" placeholder="Deposit Amount" />
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline" id="wei" type="text" placeholder="Enter Amount(WEI)" />
         </div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" onClick={newContract}>
+        <button className="bg-purple-600 hover:bg-purple-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" onClick={newContract}>
           Deploy
         </button>
       </div>
-
+    
       <div>
-        <h1 className="text-xl font-bold mb-4">Existing Contracts</h1>
-        <div>
-          {escrows.map((escrow) => (
-            <div key={escrow.address} className="bg-white shadow-md rounded px-8 py-6 mb-4">
-              <Escrow {...escrow} />
-              <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full mb-2" onClick={() => escrow.handleDelete(escrow.address)}>
-                Delete Contract
-              </button>
-              <button className={`bg-blue-500 ${escrow.isTimelockSet ? 'bg-opacity-50 cursor-not-allowed' : 'hover:bg-blue-700'} text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full`} onClick={() => escrow.handleSetTimelock(escrow.address)}>
-                {escrow.isTimelockSet ? 'Timelocked' : 'Set Timelock'}
-              </button>
-              {disableApproveButton && (
-                <p className="text-red-500 mt-2">Approve button disabled for 1 minute</p>
-              )}
-            </div>
+  <h1 className="text-3xl font-semibold mb-6 text-gray-700">List of All the Contracts</h1>
+  <div className="grid gap-4">
+    {escrows.map((escrow) => (
+      <div key={escrow.address} className="bg-gray-100 shadow-md rounded-lg p-6">
+        <Escrow {...escrow} />
+        <div className="flex justify-between mt-4">
+          <button className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={() => escrow.handleDelete(escrow.address)}>
+            Delete Contract
+          </button>
+          <button className={`bg-purple-500 hover:bg-purple-600 ${escrow.isTimelockSet ? 'bg-opacity-50 cursor-not-allowed' : 'hover:bg-yellow-600'} text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline`} onClick={() => escrow.handleSetTimelock(escrow.address)}>
+            {escrow.isTimelockSet ? 'Timelocked' : 'Set Timelock'}
+          </button>
+        </div>
+        {disableApproveButton && (
+          <p className="text-red-500 mt-4">Approve button disabled for 1 minute</p>
+        )}
+      </div>
           ))}
         </div>
       </div>
-
+    
       {loading && (
-        <div className="flex items-center justify-center mt-4">
-          <SyncLoader color={'#36D7B7'} loading={loading} size={15} />
-          <p className="ml-2">Approving...</p>
+        <div className="flex items-center justify-center mt-2">
+          <p className="ml-2 text-gray-700">Approving...</p>
         </div>
       )}
-
+    
       {showConfirmationModal && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-50">
-          <div className="bg-white shadow-md rounded px-8 py-6">
-            <p className="text-lg font-bold mb-4">Are you sure you want to delete this contract?</p>
+          <div className="bg-gray-200 shadow-lg rounded-lg px-6 py-8">
+            <p className="text-lg font-semibold mb-4 text-gray-700">Are you sure you want to delete this contract?</p>
             <div className="flex justify-end">
-              <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2 focus:outline-none focus:shadow-outline" onClick={handleConfirmDelete}>Yes</button>
-              <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={closeConfirmationModal}>No</button>
+              <button className="bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded mr-2 focus:outline-none focus:shadow-outline" onClick={handleConfirmDelete}>Yes</button>
+              <button className="bg-gray-600 hover:bg-gray-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={closeConfirmationModal}>No</button>
             </div>
           </div>
         </div>
       )}
-
+    
       {showTimelockModal && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-50">
-          <div className="bg-white shadow-md rounded px-8 py-6">
-            <p className="text-lg font-bold mb-4">Set Timelock</p>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Unlock Time (Unix Timestamp)" value={timelockValue} onChange={(e) => setTimelockValue(e.target.value)} />
+          <div className="bg-gray-200 shadow-lg rounded-lg px-6 py-8">
+            <p className="text-lg font-semibold mb-4 text-gray-700">Set Timelock</p>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Enter Unlock Time (Unix Timestamp)" value={timelockValue} onChange={(e) => setTimelockValue(e.target.value)} />
             <div className="flex justify-end mt-4">
-              <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2 focus:outline-none focus:shadow-outline" onClick={closeTimelockModal}>Set</button>
-              <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={closeTimelockModal}>Cancel</button>
+              <button className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded mr-2 focus:outline-none focus:shadow-outline" onClick={closeTimelockModal}>Set</button>
+              <button className="bg-gray-600 hover:bg-gray-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={closeTimelockModal}>Cancel</button>
             </div>
             {showTimeSetPrompt && (
-              <p className="text-green-500 mt-2">Time set successfully!</p>
+              <p className="text-green-500 mt-4">Time set successfully!</p>
             )}
           </div>
         </div>
